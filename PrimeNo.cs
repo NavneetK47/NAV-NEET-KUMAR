89b0a2a29970e7a1de78f165a6bc57ceef167631
NavneetK47 <111053262+NavneetK47@users.noreply.github.com>
2022-08-11 15:44:26 +0530

    Add files via upload

---
+++ PrimeNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter the number");
+            int num = System.Convert.ToInt32(System.Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <=num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+                if (count == 2)
+                    System.Console.WriteLine("Prime No");
+                else
+                    System.Console.WriteLine("Not Prime No");
+            
+             System.Console.ReadLine();
+        }
+    }
+}
