0d28c773152897624061cfc1d6dfa6ee19b03888
NavneetK47 <111053262+NavneetK47@users.noreply.github.com>
2022-08-11 15:56:31 +0530

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿class Demo
+{
+    static void Main()
+    {
+        int a = 0, b = 1;
+        System.Console.WriteLine(a);
+        System.Console.WriteLine(b);
+        int c;
+        for(int i=2; i<=10; i++)
+        {
+            c = a + b;
+            System.Console.WriteLine(c);
+            a = b;
+            b = c;
+        }
+        System.Console.ReadLine();
+    }
+}
