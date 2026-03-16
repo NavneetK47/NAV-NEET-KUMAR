b8beb5119f59923e4ce07a4b8a6b9ac8119a5112
NavneetK47 <kumarnavneet9792@gmail.com>
2026-03-16 13:33:13 +0530

    nk.py modify with databricks code

--- nk.py
+++ nk.py
@@ -1,2 +1,22 @@
-print("Sachin")
-print("Navneet")
+number = int(input("Enter a number"))
+c=number*number
+print(c)
+number1 = int(input("Enter first number"))
+number2 = int(input("Enter second number"))
+c = number1 + number2
+print(c)
+a = int(input("Enter value of a  number"))
+b = int(input("Enter value of b number"))
+temp=a
+a=b
+b=temp
+print("after swping a",a)
+print("after swaping b",b)
+p = int (input("Enter  value of P"))
+r = float(input("Enter value of R"))
+t = float(input("Enter value of t"))
+SI = p*r*t/100
+print("Simple interst", SI)
+r = float(input("Enter value of R"))
+a =3.14*r*r
+print("area of circle",a)
